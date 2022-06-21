ff5dda18168987ff35d3000e6c1233cc1bf0dc92
louise ouldhaddi <ouldhaddi@gmail.com>
2022-06-21 16:31:22 +0200

    boat 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,28 +5,24 @@
       <tbody>
         <tr v-for="b in board" v-bind:key="b">
           <td v-for="c in b" v-bind:key="c">
+            <!-- au click tentative devient true pour afficher ce qu'il y a dans la case -->
             <div v-on:click="c.tentative = true">
+              <!-- si tentative est true alors on affiche -->
               <div v-if="c.tentative == true">
-                {{c.boat}}
+                <!-- si il y avait un bateau alors on affiche le bateau -->
+                <div v-if="c.boat == true">
+                  <p>boat</p>
+                </div>
+                <!-- sinon on signifie l'erreur -->
+                <div v-else>X</div>
               </div>
+              <!-- si tentative est false alors on affiche rien -->
               <div v-else>~</div>
             </div>
           </td>
         </tr>
       </tbody>
     </table>
-    <!-- <table class="">
-      <tbody>
-        <tr>
-          <td class="">1</td>
-          <td class="">2</td>
-        </tr>
-        <tr>
-          <td class="">3</td>
-          <td class="">4</td>
-        </tr>
-      </tbody>
-    </table> -->
   </div>
 </template>
 
@@ -48,6 +44,8 @@ export default {
   methods: {
 
     initBoard: function(){
+
+      // création de la grille de jeu
       let res = []
       for (let index = 0; index < 10; index++) {
         let array = []
@@ -56,17 +54,49 @@ export default {
         }
         res.push(array)
       }
+
+      // création d'un bateau de 2 cases placement random
       let randomRow = Math.floor(Math.random() * 9-1) + 1
       let randomColumn = Math.floor(Math.random() * 9-1) + 1
-      res[randomRow][randomColumn].tentative = true
+      // res[randomRow][randomColumn].tentative = true
+      res[randomRow][randomColumn].boat = true
       let cel2 = Math.floor(Math.random() * 2)
       if (cel2 === 0) {
-        res[randomRow][randomColumn + 1].tentative = true
+        // res[randomRow][randomColumn + 1].tentative = true
+        res[randomRow][randomColumn + 1].boat = true
       } else  {
-        res[randomRow + 1][randomColumn].tentative = true
+        // res[randomRow + 1][randomColumn].tentative = true
+        res[randomRow + 1][randomColumn].boat = true
+      }
+
+      // création d'un bateau de 3 cases placement random
+      // reste à gérer le cas pour être sûr que les bateaux ne tombent pas sur la même case
+      let rowCel1Boat3 = Math.floor(Math.random() * 9-1) + 1
+      let columnCel1Boat3 = Math.floor(Math.random() * 9-1) + 1
+      let cel1Boat3 = res[rowCel1Boat3][columnCel1Boat3]
+      // cel1Boat3.tentative = true
+      cel1Boat3.boat = true
+
+      let r = Math.floor(Math.random() * 2)
+      if (r === 0) {
+        let cel2Boat3 = res[rowCel1Boat3][columnCel1Boat3 + 1]
+        let cel3Boat3 = res[rowCel1Boat3][columnCel1Boat3 + 2]
+        // cel2Boat3.tentative = true
+        cel2Boat3.boat = true
+        // cel3Boat3.tentative = true
+        cel3Boat3.boat = true
+
+      } else  {
+        let cel2Boat3 = res[rowCel1Boat3 + 1][columnCel1Boat3]
+        let cel3Boat3 = res[rowCel1Boat3 + 2][columnCel1Boat3]
+        // cel2Boat3.tentative = true
+        cel2Boat3.boat = true
+        // cel3Boat3.tentative = true
+        cel3Boat3.boat = true
       }
       return res
-    }
+    },
+
   }
 };
 </script>
@@ -78,10 +108,11 @@ export default {
 
   td {
     border: 1px solid black ;
+    width: 55px;
+    height: 55px;
   }
 
   table {
-    margin-left: auto;
-    margin-right: auto;
+    margin: auto;
   }
 </style>
