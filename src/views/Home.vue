1cafd3f1fa128e65e879a5ee9dac7325072f7ee8
louise ouldhaddi <ouldhaddi@gmail.com>
2022-06-22 19:31:26 +0200

    method new boat

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
 
     <p v-show="isWin()">You Win!</p>
-    <button v-on:click="board = initBoard()">start</button>
+    <button v-on:click="initBoard()">start</button>
     <p>counter: {{counter}}</p>
     {{win}}
     <table>
@@ -41,7 +41,7 @@ export default {
   name: "Home",
   data: function () {
     return {
-      board: this.initBoard(),
+      board: [],
       counter: 0,
       win: 0,
     };
@@ -53,56 +53,79 @@ export default {
       // création de la grille de jeu
       this.counter = 0
       this.win = 0
-      let res = []
+      this.board = []
       for (let index = 0; index < 10; index++) {
         let array = []
         for (let i = 0; i < 10; i++) {
           array.push(new Cell());
         }
-        res.push(array)
+        this.board.push(array)
       }
 
+      this.newBoat(2)
       // création d'un bateau de 2 cases placement random
       let randomRow = Math.floor(Math.random() * 9-1) + 1
       let randomColumn = Math.floor(Math.random() * 9-1) + 1
-      res[randomRow][randomColumn].tentative = true
-      res[randomRow][randomColumn].boat = true
+      this.board[randomRow][randomColumn].tentative = true
+      this.board[randomRow][randomColumn].boat = true
       let cel2 = Math.floor(Math.random() * 2)
       if (cel2 === 0) {
-        res[randomRow][randomColumn + 1].tentative = true
-        res[randomRow][randomColumn + 1].boat = true
+        this.board[randomRow][randomColumn + 1].tentative = true
+        this.board[randomRow][randomColumn + 1].boat = true
       } else  {
-        res[randomRow + 1][randomColumn].tentative = true
-        res[randomRow + 1][randomColumn].boat = true
+        this.board[randomRow + 1][randomColumn].tentative = true
+        this.board[randomRow + 1][randomColumn].boat = true
       }
 
       // création d'un bateau de 3 cases placement random
       // reste à gérer le cas pour être sûr que les bateaux ne tombent pas sur la même case
       let rowCel1Boat3 = Math.floor(Math.random() * 9-2) + 2
       let columnCel1Boat3 = Math.floor(Math.random() * 9-2) + 2
-      let cel1Boat3 = res[rowCel1Boat3][columnCel1Boat3]
+      let cel1Boat3 = this.board[rowCel1Boat3][columnCel1Boat3]
       // cel1Boat3.tentative = true
       cel1Boat3.boat = true
 
       let r = Math.floor(Math.random() * 2)
       if (r === 0) {
-        let cel2Boat3 = res[rowCel1Boat3][columnCel1Boat3 + 1]
-        let cel3Boat3 = res[rowCel1Boat3][columnCel1Boat3 + 2]
+        let cel2Boat3 = this.board[rowCel1Boat3][columnCel1Boat3 + 1]
+        let cel3Boat3 = this.board[rowCel1Boat3][columnCel1Boat3 + 2]
         // cel2Boat3.tentative = true
         cel2Boat3.boat = true
         // cel3Boat3.tentative = true
         cel3Boat3.boat = true
 
       } else  {
-        let cel2Boat3 = res[rowCel1Boat3 + 1][columnCel1Boat3]
-        let cel3Boat3 = res[rowCel1Boat3 + 2][columnCel1Boat3]
+        let cel2Boat3 = this.board[rowCel1Boat3 + 1][columnCel1Boat3]
+        let cel3Boat3 = this.board[rowCel1Boat3 + 2][columnCel1Boat3]
         // cel2Boat3.tentative = true
         cel2Boat3.boat = true
         // cel3Boat3.tentative = true
         cel3Boat3.boat = true
       }
-      return res
     },
+
+    isVertical() {
+      return Math.floor(Math.random() * 2) === 0
+    },
+
+    // n étant le nombre de case du bateau
+    newBoat(n) {
+      // TODO: 0 ne devrait pas être possible
+      let x1 = Math.floor(Math.random() * 9-(n-1)) + (n-1)
+      let y1 = Math.floor(Math.random() * 9-(n-1)) + (n-1)
+      console.log(x1, y1)
+      this.board[x1][y1].boat = true
+      this.board[x1][y1].tentative = true
+
+      if (this.isVertical()) {
+        // exemple pour 3 case
+        // this.board[x1][y1 + 1].boat = true
+        // this.board[x1][y1 + 1].tentative = true
+        // this.board[x1][y1 + 2].boat = true
+        // this.board[x1][y1 + 2].tentative = true
+      }
+    },
+
 
     isWin() {
       // for (const row of this.board) {
